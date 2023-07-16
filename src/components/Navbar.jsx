2f3c681ec4e4deb8f0ddb02f13fa907b3c490252
doniurek <aldonaglebicka@gmail.com>
2023-07-16 23:43:02 +0200

    Add smooth scrolling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
+import {Link} from 'react-scroll'
 
 function Navbar() {
   const [nav, setNav] = useState(true)
@@ -12,10 +13,18 @@ function Navbar() {
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-[#F4F4EF]">
       <h1 className="w-full text-3xl font-bold text-[#A01D26]">Cars Masters</h1>
       <ul className="hidden md:flex">
-        <li className="p-4">Home</li>
-        <li className="p-4">Offer</li>
-        <li className="p-4">Opinions</li>
-        <li className="p-4">Contact</li>
+      <li className="p-4">
+          <Link to="home" smooth={true} duration={500} className='cursor-pointer hover:text-[#ACBEBE]'>Home</Link>
+        </li>
+        <li className="p-4">
+          <Link to="offer" smooth={true} duration={500} className='cursor-pointer hover:text-[#ACBEBE]'>Offer</Link>
+        </li>
+        <li className="p-4">
+          <Link to="opinions" smooth={true} duration={500} className='cursor-pointer hover:text-[#ACBEBE]'>Opinions</Link>
+        </li>
+        <li className="p-4">
+          <Link to="contact" smooth={true} duration={500} className='cursor-pointer hover:text-[#ACBEBE]'>Contact</Link>
+        </li>
       </ul>
       <div onClick={handleNav} className='block md:hidden'>
         {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
